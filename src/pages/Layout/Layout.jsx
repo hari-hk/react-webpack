95b62bf0337a67c87073de046cef338c71535fb4
Hari Hara Kumar <hariharakumar5196@gmail.com>
2021-04-25 18:35:49 +0530

    Initial commit

---
+++ src/pages/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header/Header.jsx";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Layout;
