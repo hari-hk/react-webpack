e3071e476dfed7bcd9d4df8040e78d69fddf501b
hari hara kumar <hariharakumar5196@gmail.com>
2021-06-16 22:48:52 +0530

    refactor: 💡 remove unused code

--- src/components/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Header.scss";
-function Header() {
-  return <header className="header">Header</header>;
-}
-
-export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -33,5 +33,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
 };
