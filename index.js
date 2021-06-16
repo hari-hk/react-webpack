696049a27e0cd9d662bc91f0acd30de1d26ea936
hari hara kumar <hariharakumar5196@gmail.com>
2021-06-16 23:25:06 +0530

    feat: 🎸 add lint
    add eslint and prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./src/App.jsx";
-import "./style.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src/App.jsx';
+import './style.scss';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // Where files should be sent once they are bundled
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index.js",
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.js',
   },
   // webpack 5 comes with devServer which loads in development mode
   devServer: {
@@ -19,19 +19,19 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /nodeModules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.(css|scss)$/,
         exclude: /nodeModules/,
         use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "postcss-loader" },
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
         ],
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
 };
