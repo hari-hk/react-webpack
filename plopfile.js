01c52b6fa561a279cd69a8b3a11faac617157689
hari hara kumar <hariharakumar5196@gmail.com>
2021-06-26 22:31:43 +0530

    fix: 🐛 remove hooks and injectable plops

--- plopfile.js
+++ plopfile.js
@@ -99,41 +99,4 @@ module.exports = (plop) => {
       },
     ],
   });
-
-  plop.setGenerator('hook', {
-    description: 'Create a custom react hook',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is your hook name?',
-        validate: requireField('name'),
-      },
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/hooks/{{camelCase name}}.js',
-        templateFile: 'plop-templates/hook.js.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/hooks/index.js',
-        templateFile: 'plop-templates/injectable-index.js.hbs',
-        skipIfExists: true,
-      },
-      {
-        type: 'append',
-        path: 'src/hooks/index.js',
-        pattern: `/* PLOP_INJECT_IMPORT */`,
-        template: `import {{camelCase name}} from './{{camelCase name}}';`,
-      },
-      {
-        type: 'append',
-        path: 'src/hooks/index.js',
-        pattern: `/* PLOP_INJECT_EXPORT */`,
-        template: `\t{{camelCase name}},`,
-      },
-    ],
-  });
 };
